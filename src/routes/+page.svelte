ee5e29d1c9823a4ce9d33d383c0f40a57f49e2ff
h4ck0rman <h4ck0rman@gmail.com>
2023-11-05 02:52:12 -0500

    Installed and integrated Prisma DB

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+    $: console.log(data)
+
+    const logs = data.data
+    console.log(logs)
+</script>
+
+<h1>ShulkBred</h1>
+{#each logs.data as log}
+    <p>{log.state} {log.weight} {log.date}</p>
+{/each}
