0b11bc8558b5fc47f1975a130758deea97700367
h4ck0rman <h4ck0rman@gmail.com>
2023-11-07 06:44:49 -0500

    Pink sasook added

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,11 +1,11 @@
 <script>
-    let src = "./sasook.png" 
+    let src = "./sasookPink.png" 
 </script>
 
 <div class="flex justify-between items-center py-2">
     
     <a href="/" class="flex items-center px-1 mx-4 rounded-full overflow-hidden">
-        <img {src} alt="Icon" class="h-auto w-10 object-contain"/>
+        <img {src} alt="Icon" class="h-auto w-10"/>
     </a>
     
     <div class="flex justify-center">
--- src/routes/tracker/+page.svelte
+++ src/routes/tracker/+page.svelte
@@ -10,16 +10,19 @@
 
 <div class="relative">
     <img src={sakuras} alt="Sakuras Japanese Art Wallpaper"/>  
-    <form method="POST" action="?/create" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 rounded-full bg-white overflow-hidden flex justify-around">
+    <form method="POST" action="?/create" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 rounded-full bg-white overflow-hidden flex justify-between">
+        <div>
         <select name="state" placeholder="Bulking" class="bg-white outline-none py-3 m-1">
             <option value="Bulking">Bulking</option>
             <option value="Shredding">Shredding</option>
             <option value="Maintenance">Maintenance</option>
-            </select>
-        <input name="weight" placeholder="Weight" class="border-b-2 py-3 m-1"/>
-        <button action="submit" class="py-3 px-6 m-1 rounded-full hover:bg-pink-400 opacity-70 hover:opacity-100">
-            Enter
-        </button>
+        </select>
+        
+            <input name="weight" placeholder="Weight" class="border-b-2 py-3 m-1"/>
+        </div>
+        <button action="submit" class="py-3 px-6 m-1 rounded-full hover:bg-pink-400 opacity-70 hover:opacity-100">Enter</button>
+        
+        
     </form>
 </div>
 
