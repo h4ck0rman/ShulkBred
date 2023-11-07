f278c10f5c7a8a838fa1ad217df624d748d9c17a
h4ck0rman <h4ck0rman@gmail.com>
2023-11-07 05:44:48 -0500

    using tailwind apply for navigation buttons

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -4,13 +4,13 @@
 
 <div class="flex justify-between items-center py-2">
     
-    <a href="/" class="flex items-center px-1 mx-4 rounded-full bg-white overflow-hidden">
+    <a href="/" class="flex items-center px-1 mx-4 rounded-full overflow-hidden">
         <img {src} alt="Icon" class="h-auto w-10 object-contain"/>
     </a>
     
     <div class="flex justify-center">
-        <a href="/tracker" class="py-3 px-6 mr-1 rounded-full hover:bg-pink-300 hover:opacity-75"><button>Tracker</button></a>
-        <a href="/tracker" class="py-3 px-6 mr-1 rounded-full hover:bg-pink-300 hover:opacity-75"><button>About</button></a>
+        <a href="/tracker" class="navButton"><button>Tracker</button></a>
+        <a href="/tracker" class="navButton"><button>About</button></a>
     </div>
     
 
